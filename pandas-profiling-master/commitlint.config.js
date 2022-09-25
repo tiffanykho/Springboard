757cf7e3cc3fe7dcb1fdfecb9d4454f5da00084f
tiffanykho <109111518+tiffanykho@users.noreply.github.com>
2022-09-25 12:02:03 -0700

    Delete pandas-profiling-master directory

--- pandas-profiling-master/commitlint.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-        'body-max-line-length': [2, 'always', 120],
-        'footer-max-line-length': [2, 'always', 120],
-    },
-};
